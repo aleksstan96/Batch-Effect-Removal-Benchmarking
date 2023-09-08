4fc497d51793ead169a0470ab37e53a7d782c3f3
aleksandra <alexstan220196@gmail.com>
2023-09-08 22:35:08 +0000

    Add liger and scVI. Working version

--- benchmark.py
+++ benchmark.py
@@ -12,6 +12,7 @@ from scipy import stats
 import scib
 # import scvi
 from sklearn.metrics.cluster import adjusted_rand_score,normalized_mutual_info_score
+from scipy.sparse import csr_matrix
 
 
 def preprocessing(adata: ad.AnnData,  hvgs, scale, scale_total, norm_total=False, log1p=False, pca=False, use_highly_variable=None, n_high_var=2000, flavor="seurat", hvg_list=None, n_comps=50, batch_key="batch", target_sum=1e4, scale_value=None):
@@ -82,12 +83,13 @@ def call_harmony(adata: ad.AnnData, label_name='Harmony_emb', batch_key='batch',
     sc.tl.pca(adata, n_comps=n_comps)
 
     print("Harmony...")     
+    # only measure time for harmonize function
     t_init = time.process_time()
     adata.obsm[label_name] = harmonize(adata.obsm["X_pca"], adata.obs, batch_key=batch_key)
     t_fin = time.process_time()
     print(f'Harmonize function ran for: {t_fin-t_init} s')
-
-    return adata
+    exe_time = t_fin-t_init
+    return adata, exe_time
 
 def call_scDML(adata: ad.AnnData, label_name='scDML_emb', verbose=False, initial_res=3.0, n_comps=50, save_dir='./test_result/', batch_key='batch', ncluster=14, ncluster_list=[14], merge_rule="rule2"):
 
@@ -98,30 +100,39 @@ def call_scDML(adata: ad.AnnData, label_name='scDML_emb', verbose=False, initial
     print("scDML...")
 
     temp_ad = adata.copy()
-    t_init = time.process_time()
 
     scdml=scDMLModel(verbose=verbose, save_dir=save_dir)
     # since normalization, log1p, HVGs extraction and scaling were performed in the previous steps, we only let preprocess function to perform PCA
     adt = scdml.preprocess(temp_ad, cluster_method="louvain", resolution=initial_res, pca_dim=n_comps, normalize_samples=False, normalize_features=False, log_normalize=False, batch_key=batch_key)
+    # only measure time for integrate function
+    t_init = time.process_time()
     scdml.integrate(adt, batch_key=batch_key, ncluster_list=ncluster_list, expect_num_cluster=ncluster,merge_rule=merge_rule)
     t_fin = time.process_time()
     print(f'scDML integrate ran for: {t_fin-t_init} s')
     temp_ad.obsm[label_name] = adt.obsm["X_emb"]
     del temp_ad.obsm['X_emb']
 
+    exe_time = t_fin-t_init
     print("Adata after scDML: ", temp_ad)
-    return temp_ad
+    return temp_ad, exe_time
 
-def call_liger(adata: ad.AnnData, label_name='liger_emb', verbose=False, save_dir='./test_result/', batch_key='batch', ncluster=14, ncluster_list=[14], merge_rule="rule2"):
+def call_liger(adata: ad.AnnData, label_name='Liger_emb', batch_key='batch', hvgs=True):
 
     import pyliger
 
-    # code was taken from this tuturial: https://scib-metrics.readthedocs.io/en/stable/notebooks/lung_example.html#liger
-    bdata = adata.copy()
+    # parts of code were borrowed this tuturial: https://scib-metrics.readthedocs.io/en/stable/notebooks/lung_example.html#liger
+
+    if hvgs:
+        hvg = adata.var[adata.var.highly_variable].index.values
+        bdata = adata[:, hvg].copy()
+    else:
+        bdata = adata.copy()
+
     # Pyliger normalizes by library size with a size factor of 1
     # So here we give it the count data
-    bdata.X = bdata.layers["counts"]
+    bdata.X = csr_matrix(bdata.layers["counts"])
     # List of adata per batch
+    batch_cats = np.unique(adata.obs[batch_key].values)
     adata_list = [bdata[bdata.obs[batch_key] == b].copy() for b in batch_cats]
     for i, ad in enumerate(adata_list):
         ad.uns["sample_name"] = batch_cats[i]
@@ -134,18 +145,61 @@ def call_liger(adata: ad.AnnData, label_name='liger_emb', verbose=False, save_di
     liger_data.var_genes = bdata.var_names
     pyliger.normalize(liger_data)
     pyliger.scale_not_center(liger_data)
+    # only measure time for optimize_ALS and quantile_norm
+    t_init = time.process_time()
     pyliger.optimize_ALS(liger_data, k=30)
     pyliger.quantile_norm(liger_data)
+    t_fin = time.process_time()
 
+    exe_time = t_fin-t_init
 
     adata.obsm[label_name] = np.zeros((adata.shape[0], liger_data.adata_list[0].obsm["H_norm"].shape[1]))
     for i, b in enumerate(batch_cats):
-        adata.obsm[label_name][adata.obs.batch == b] = liger_data.adata_list[i].obsm["H_norm"]
+        adata.obsm[label_name][adata.obs[batch_key] == b] = liger_data.adata_list[i].obsm["H_norm"]
 
-    return adata
+    return adata, exe_time
+
+def call_scVI(adata: ad.AnnData, label_name='scVI_emb', batch_key='batch', max_epochs=None, hvgs=True, raw_count_layer='counts'):
+    # scVI models raw counts directly, so it is important that we provide it with a count matrix rather than a normalized expression matrix.
+    import scvi
+    from scvi.model import SCVI
+    scvi.settings.seed = 0
+
+    # code was borrowed from scVI official tutorial: https://docs.scvi-tools.org/en/stable/tutorials/notebooks/scrna/harmonization.html#dataset-preprocessing
+
+    n_latent = 30
+    n_hidden = 128
+    n_layers = 2
+
+    # copying to not return values added to adata during setup_anndata
+    net_adata = adata.copy()
+    if hvgs:
+        hvg = net_adata.var[net_adata.var.highly_variable].index.values
+        net_adata = adata[:, hvg].copy()
+
+    # only measure time for training
+    t_init = time.process_time()
+    SCVI.setup_anndata(net_adata, layer=raw_count_layer, batch_key=batch_key)
+
+    vae = SCVI(
+        net_adata,
+        gene_likelihood="nb",
+        n_layers=n_layers,
+        n_latent=n_latent,
+        n_hidden=n_hidden,
+    )
+    train_kwargs = {"train_size": 1.0}
+    if max_epochs is not None:
+        train_kwargs["max_epochs"] = max_epochs
+    t_fin = time.process_time()
+    vae.train(**train_kwargs)
+    adata.obsm[label_name] = vae.get_latent_representation()
+
+    exe_time = t_fin-t_init
+    return adata, exe_time
 
 
-def save_results_to_dataframe(dataset, algorithm, hvgs, scaled_batch, scaled_total, graph_iLISI, graph_cLISI, ARI, NMI, dataframe=None, path='/goofys/users/Aleksandra_S/benchmarking_datasets/results/', file_name_sufix = ''):
+def save_results_to_dataframe(dataset, algorithm, hvgs, scaled_batch, scaled_total, graph_iLISI, graph_cLISI, ARI, NMI, exe_time, dataframe=None, path='/goofys/users/Aleksandra_S/benchmarking_datasets/results/', file_name_sufix = ''):
     
     file_path = path + 'result_table_test.csv'
 
@@ -156,7 +210,7 @@ def save_results_to_dataframe(dataset, algorithm, hvgs, scaled_batch, scaled_tot
     if dataframe is None:
         dataframe = pd.DataFrame(columns=['dataset', 'algorithm', 'hvgs', 'scaled batch', 
         # 'scaled total', 
-        'graph iLISI', 'graph cLISI', 'ARI', 'NMI'])
+        'graph iLISI', 'graph cLISI', 'ARI', 'NMI', 'Execution time'])
     
     results_dict = {
         'dataset': dataset,
@@ -167,7 +221,8 @@ def save_results_to_dataframe(dataset, algorithm, hvgs, scaled_batch, scaled_tot
         'graph iLISI': graph_iLISI,
         'graph cLISI': graph_cLISI,
         'ARI': ARI,
-        'NMI': NMI
+        'NMI': NMI,
+        'Execution time': exe_time
     }
     
     dataframe = dataframe.append(results_dict, ignore_index=True)
@@ -190,14 +245,23 @@ def main(path, params):
     adata = preprocessing(adata, norm_total= params.get('norm_total'),  log1p= params.get('log1p'), hvgs=params.get('hvgs'), flavor=params.get('flavor'), scale=params.get('scale'), scale_total=params.get('scale_total'), batch_key=params.get('batch_key'), n_comps=params.get('n_comps'))
     # calling batch-removal method
     if params.get('algo').lower()=='harmony':
-        adata = call_harmony(adata, batch_key=params.get('batch_key'), n_comps=params.get('n_comps'), label_name=use_rep)
+        adata, exe_time = call_harmony(adata, batch_key=params.get('batch_key'), n_comps=params.get('n_comps'), label_name=use_rep)
     elif params.get('algo').lower()=='scdml':
-        adata = call_scDML(adata, batch_key=params.get('batch_key'), n_comps=params.get('n_comps'), label_name=use_rep)
+        adata, exe_time = call_scDML(adata, batch_key=params.get('batch_key'), n_comps=params.get('n_comps'), label_name=use_rep)
+    elif params.get('algo').lower()=='liger':
+        # for liger we should not perform scaling (it will perform its own without zero-centering the data)
+        adata, exe_time = call_liger(adata, batch_key=params.get('batch_key'), label_name=use_rep, hvgs=params.get('hvgs'))
+    elif params.get('algo').lower()=='scvi':
+        # scVI models raw counts directly, so it is important that we provide it with a count matrix rather than a normalized expression matrix.
+        # for scVI we should perform no scaling
+        adata, exe_time = call_scVI(adata, batch_key=params.get('batch_key'), label_name=use_rep, raw_count_layer='counts', hvgs=params.get('hvgs'))
+
     # performing clustering
     # resolutions = np.arange(params.get('resolutions_min'), params.get('resolutions_max'), params.get('resolutions_step'))
     # for r in resolutions:
     #     adata = clustering(adata, reso=r)
     #     key = "louvain"
+    
     adata = clustering(adata, cluster_method=params.get('clust_algo'), reso=params.get('reso'), use_rep=use_rep)
     # calculating metrics
     ari=adjusted_rand_score(adata.obs[params.get('clust_algo')], adata.obs[params.get('cell_type_key')])
@@ -216,12 +280,12 @@ def main(path, params):
     print("Parameters: ")
     print(params)
 
-    save_results_to_dataframe(dataset=params.get('dataset'), algorithm=params.get('algo'), hvgs=params.get('hvgs'), scaled_batch=params.get('scale'), scaled_total=params.get('scale_total'), graph_iLISI=ilisi, graph_cLISI=clisi, ARI=ari, NMI=nmi)
+    save_results_to_dataframe(dataset=params.get('dataset'), algorithm=params.get('algo'), hvgs=params.get('hvgs'), scaled_batch=params.get('scale'), scaled_total=params.get('scale_total'), graph_iLISI=ilisi, graph_cLISI=clisi, ARI=ari, NMI=nmi, exe_time=exe_time)
 
 if __name__ == "__main__":
     params_list = [
     {
-        'algo' : 'scDML',
+        'algo' : 'scVI',
         'dataset' : 'Human_pancreas_norm_complexBatch',
         'hvgs' : False,
         'flavor' : 'cell_ranger',
@@ -240,28 +304,28 @@ if __name__ == "__main__":
         'n_comps' : 20,
         'graph_ilisi_k0' : 31,
     },
+    # {
+    #     'algo' : 'scVI',
+    #     'dataset' : 'Human_pancreas_norm_complexBatch',
+    #     'hvgs' : False,
+    #     'flavor' : 'cell_ranger',
+    #     'scale' : True,
+    #     'scale_total' : False,
+    #     'norm_total' : False,
+    #     'log1p' : False,
+    #     'pca' : False,
+    #     'resolutions_min' : 0.1,
+    #     'resolutions_max' : 1.1,
+    #     'resolutions_step' : 0.1,
+    #     'reso' : 1,
+    #     'clust_algo' : 'louvain',
+    #     'batch_key' : 'tech',
+    #     'cell_type_key' : 'celltype',
+    #     'n_comps' : 20,
+    #     'graph_ilisi_k0' : 31,
+    # },
     {
-        'algo' : 'scDML',
-        'dataset' : 'Human_pancreas_norm_complexBatch',
-        'hvgs' : False,
-        'flavor' : 'cell_ranger',
-        'scale' : True,
-        'scale_total' : False,
-        'norm_total' : False,
-        'log1p' : False,
-        'pca' : False,
-        'resolutions_min' : 0.1,
-        'resolutions_max' : 1.1,
-        'resolutions_step' : 0.1,
-        'reso' : 1,
-        'clust_algo' : 'louvain',
-        'batch_key' : 'tech',
-        'cell_type_key' : 'celltype',
-        'n_comps' : 20,
-        'graph_ilisi_k0' : 31,
-    },
-    {
-        'algo' : 'scDML',
+        'algo' : 'scVI',
         'dataset' : 'Human_pancreas_norm_complexBatch',
         'hvgs' : True,
         'flavor' : 'cell_ranger',
@@ -280,26 +344,26 @@ if __name__ == "__main__":
         'n_comps' : 20,
         'graph_ilisi_k0' : 31,
     },
-    {
-        'algo' : 'scDML',
-        'dataset' : 'Human_pancreas_norm_complexBatch',
-        'hvgs' : True,
-        'flavor' : 'cell_ranger',
-        'scale' : True,
-        'scale_total' : False,
-        'norm_total' : False,
-        'log1p' : False,
-        'pca' : False,
-        'resolutions_min' : 0.1,
-        'resolutions_max' : 1.1,
-        'resolutions_step' : 0.1,
-        'reso' : 1,
-        'clust_algo' : 'louvain',
-        'batch_key' : 'tech',
-        'cell_type_key' : 'celltype',
-        'n_comps' : 20,
-        'graph_ilisi_k0' : 31,
-    },
+    # {
+    #     'algo' : 'scVI',
+    #     'dataset' : 'Human_pancreas_norm_complexBatch',
+    #     'hvgs' : True,
+    #     'flavor' : 'cell_ranger',
+    #     'scale' : True,
+    #     'scale_total' : False,
+    #     'norm_total' : False,
+    #     'log1p' : False,
+    #     'pca' : False,
+    #     'resolutions_min' : 0.1,
+    #     'resolutions_max' : 1.1,
+    #     'resolutions_step' : 0.1,
+    #     'reso' : 1,
+    #     'clust_algo' : 'louvain',
+    #     'batch_key' : 'tech',
+    #     'cell_type_key' : 'celltype',
+    #     'n_comps' : 20,
+    #     'graph_ilisi_k0' : 31,
+    # },
     ]
 
     t_init = time.process_time()
